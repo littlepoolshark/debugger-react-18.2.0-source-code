b550da2ef5bf51576cf1e6b6f20f018e69e7925e
Sam Liu <liuqishen@dayuwuxian.com>
2023-03-01 15:58:52 +0800

    feat: add config file for vscode debug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => setCount((c) => c + 1)}>{count}</button>
       </header>
     </div>
   );
